d6ebaf7f97d8b6f56628b98a45dfdd9256b9b06c
Ned Palacios <npdoesmc@gmail.com>
2022-01-18 19:53:56 +0800

    fix submit id modal

--- frontend/src/components/SubmitIDModal.vue
+++ frontend/src/components/SubmitIDModal.vue
@@ -1,7 +1,7 @@
 <template>
   <modal title="Set an ID" :open="$store.state.isIDModalOpen" @update:open="commit('SET_ID_MODAL_OPEN', $event)">
     <form @submit.prevent="submitIDForm" class="flex flex-col">
-      <div class="form-control">
+      <div class="form-control" name="associated_id_field">
         <label class="label">
           <span class="label-text">Recipient student ID</span>
         </label>
@@ -21,8 +21,10 @@ export default {
   methods: {
     async submitIDForm(e: SubmitEvent) {
       if (!e.target || !(e.target instanceof HTMLFormElement)) return;
-      const associatedIdField = e.target.children.namedItem('associated_id');
-      if (!associatedIdField || !(associatedIdField instanceof HTMLInputElement)) return;
+      const associatedIdField = e.target.children.namedItem('associated_id_field');
+      if (!associatedIdField) return;
+      const associatedIdTextField = associatedIdField.children.namedItem('associated_id');
+      if (!associatedIdTextField || !(associatedIdTextField instanceof HTMLInputElement)) return;
 
       try {
         const resp = await fetch(import.meta.env.VITE_BACKEND_URL + '/user/connect_id', {
@@ -32,14 +34,14 @@ export default {
             ...this.$store.getters.headers
           },
           body: JSON.stringify({
-            associated_id: associatedIdField.value,
+            associated_id: associatedIdTextField.value,
           })
         });
 
         const json = await resp.json();
         if (resp.status >= 200 && resp.status <= 299) {
           notify(this, { type: 'success', text: json['message'] });
-          this.$store.commit('SET_USER_ASSOCIATED_ID', associatedIdField.value);
+          this.$store.commit('SET_USER_ASSOCIATED_ID', associatedIdTextField.value);
           this.$store.commit('SET_ID_MODAL_OPEN', false);
         } else if ('error_message' in json) {
           throw new Error(json['error_message']);
