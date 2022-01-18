30c47931c8a1414ef4592deb43a5866246b323d6
Ned Palacios <npdoesmc@gmail.com>
2022-01-18 11:55:48 +0800

    move frontend stuff to frontend/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import WindiCSS from 'vite-plugin-windicss'
 import Icons from 'unplugin-icons/vite'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Icons()
+    Icons(),
+    checker({typescript: true})
   ]
 })
