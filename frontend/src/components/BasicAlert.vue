486037dce1e603a700ad4d6f2db66c2538d5f789
Ned Palacios <npdoesmc@gmail.com>
2022-01-19 19:31:29 +0800

    make alert message centered

--- frontend/src/components/BasicAlert.vue
+++ frontend/src/components/BasicAlert.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="alert rounded-md text-center" :class="{ 
+  <div class="alert rounded-md justify-center" :class="{ 
     'bg-orange-50 border-red-500 text-red-600': type == 'error', 
     'bg-blue-50 border-blue-500 text-blue-600': type == 'info', 
     'bg-green-50 border-teal-500 text-teal-600': type == 'success', 
     'bg-amber-50 border-orange-500 text-orange-600': type == 'warning' 
   }">
-    <p>{{ message }}</p>
+    <p class="text-center">{{ message }}</p>
   </div>
 </template>
 
