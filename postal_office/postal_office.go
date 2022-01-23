6000ed660b308560329cf474ca5faa0697c68122
Ned Palacios <npdoesmc@gmail.com>
2022-01-23 19:23:53 +0800

    add graceful shutdown, auto save to postal office

--- postal_office/postal_office.go
+++ postal_office/postal_office.go
@@ -8,12 +8,17 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"strconv"
 	"sync"
+	"syscall"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/mailgun/mailgun-go/v4"
 	goNanoid "github.com/matoous/go-nanoid/v2"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/nedpals/valentine-wall/postal_office/types"
 	"golang.org/x/sync/semaphore"
 )
@@ -53,6 +58,56 @@ func init() {
 		targetEnv = gotEnv
 	}
 }
+
+func dataDir() string {
+	baseDir := "."
+	if execPath, err := os.Executable(); err != nil {
+		log.Println(err)
+	} else {
+		baseDir = filepath.Dir(execPath)
+	}
+
+	return filepath.Join(baseDir, "_data")
+}
+
+func databasePath() string {
+	return filepath.Join(dataDir(), fmt.Sprintf("postal_office-%s.db", targetEnv))
+}
+
+func needOpenDB() bool {
+	if _, err := os.Stat(dataDir()); os.IsNotExist(err) {
+		return false
+	} else if _, err := os.Stat(databasePath()); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func loadDatabase() (*sqlx.DB, error) {
+	if err := os.MkdirAll(dataDir(), os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	databasePath := databasePath()
+	log.Printf("loading database from %s\n", databasePath)
+	db, err := sqlx.Open("sqlite3", databasePath)
+	if err != nil {
+		return nil, err
+	}
+
+	schema := `
+CREATE TABLE IF NOT EXISTS pending_jobs (
+id TEXT PRIMARY KEY,
+unique_id TEXT NOT NULL,
+payload TEXT NOT NULL,
+updated_at TIMESTAMP NOT NULL
+)`
+	if _, err := db.Exec(schema); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 type PostalOffice struct {
@@ -61,6 +116,14 @@ type PostalOffice struct {
 	cancelCh          chan string
 	mg                *mailgun.MailgunImpl
 	mappedPendingJobs *sync.Map
+	pendingJobsCount  int
+}
+
+func deleteJob(po *PostalOffice, uniqueId string) {
+	log.Printf("deleting job with unique id %s\n", uniqueId)
+	po.mappedPendingJobs.Delete(uniqueId)
+	po.sema.Release(1)
+	po.pendingJobsCount--
 }
 
 func (po *PostalOffice) NewJob(args *types.NewJobArgs, jobId *string) error {
@@ -72,21 +135,28 @@ func (po *PostalOffice) NewJob(args *types.NewJobArgs, jobId *string) error {
 		return err
 	}
 
-	gotJobId, err := goNanoid.New()
-	if err != nil {
-		return err
+	gotJobId := args.ID
+	if len(gotJobId) == 0 {
+		var err error
+		gotJobId, err = goNanoid.New()
+		if err != nil {
+			return err
+		}
 	}
 
-	go func(mg *mailgun.MailgunImpl, msg types.MailMessage, sendDuration time.Duration, assignedJobId string, assignedUniqueId string) {
+	timeNow := time.Now()
+	go func(po *PostalOffice, msg types.MailMessage, sendDuration time.Duration, assignedJobId string, assignedUniqueId string) {
+		defer deleteJob(po, assignedUniqueId)
+
 		for {
 			select {
 			case jobIdToCancel := <-po.cancelCh:
-				if jobIdToCancel == assignedJobId {
-					log.Printf("[%s] job cancelled", assignedJobId)
+				if jobIdToCancel == assignedJobId || jobIdToCancel == "*" {
+					log.Printf("[%s] job cancelled\n", assignedJobId)
 					return
 				}
 			case <-time.After(sendDuration):
-				mgMsg := mg.NewMessage(
+				mgMsg := po.mg.NewMessage(
 					fmt.Sprintf("%s <mailgun@%s>", msg.Name, mailgunDomain),
 					msg.Subject,
 					msg.Content,
@@ -95,22 +165,34 @@ func (po *PostalOffice) NewJob(args *types.NewJobArgs, jobId *string) error {
 
 				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 				defer cancel()
-				defer po.sema.Release(1)
-				defer po.mappedPendingJobs.Delete(assignedUniqueId)
 
-				if _, _, err := mg.Send(ctx, mgMsg); err != nil {
-					log.Printf("[%s] job error: %s", assignedJobId, err.Error())
+				if _, _, err := po.mg.Send(ctx, mgMsg); err != nil {
+					log.Printf("[%s] job error: %s\n", assignedJobId, err.Error())
 				} else {
-					log.Printf("[%s] job success", assignedJobId)
+					log.Printf("[%s] job success\n", assignedJobId)
 				}
 
 				return
 			}
 		}
-	}(po.mg, args.Payload, args.After, gotJobId, args.UniqueID)
+	}(po, args.Payload, args.After, gotJobId, args.UniqueID)
 
 	*jobId = gotJobId
-	po.mappedPendingJobs.Store(args.UniqueID, gotJobId)
+	pendingJob := &types.PendingJob{
+		ID:       gotJobId,
+		UniqueID: args.UniqueID,
+		Payload: &types.JobPayload{
+			Type:      args.Type,
+			SendAfter: args.After,
+			Message:   args.Payload,
+		},
+		UpdatedAt: timeNow,
+	}
+
+	pendingJob.Payload.ParentJob = pendingJob
+	po.mappedPendingJobs.Store(args.UniqueID, pendingJob)
+	po.pendingJobsCount++
+
 	log.Printf("new job received: %s\n", gotJobId)
 	return nil
 }
@@ -123,12 +205,92 @@ func (po *PostalOffice) CancelJob(args *types.CancelJobArgs, ok *bool) error {
 }
 
 func (po *PostalOffice) GetJobID(args *types.GetJobIDArgs, jobId *string) error {
-	gotJobId, exists := po.mappedPendingJobs.Load(args.UniqueID)
+	rawPendingJob, exists := po.mappedPendingJobs.Load(args.UniqueID)
 	if !exists {
 		return fmt.Errorf("no job id for specific unique id found")
 	}
-	*jobId = gotJobId.(string)
+	*jobId = rawPendingJob.(*types.PendingJob).ID
 	return nil
+}
+
+func loadPendingJobs(po *PostalOffice) error {
+	if !needOpenDB() {
+		log.Println("skipping loading pending jobs")
+		return nil
+	}
+
+	db, err := loadDatabase()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+	pendingJobs := []*types.PendingJob{}
+	if err := db.Select(&pendingJobs, "SELECT * FROM pending_jobs"); err != nil {
+		return err
+	}
+
+	success := 0
+	for _, pj := range pendingJobs {
+		var jobId string
+		err := po.NewJob(&types.NewJobArgs{
+			Type:     pj.Payload.Type,
+			UniqueID: pj.UniqueID,
+			ID:       pj.ID,
+			After:    pj.Payload.RemainingSendAfter,
+			Payload:  pj.Payload.Message,
+		}, &jobId)
+		if err != nil {
+			log.Println(err)
+		} else {
+			success++
+		}
+	}
+
+	db.MustExec("DELETE FROM pending_jobs")
+	log.Printf("%d jobs were loaded successfully\n", success)
+	return nil
+}
+
+func savePendingJobs(po *PostalOffice) error {
+	log.Println("saving pending jobs...")
+	if po.pendingJobsCount == 0 {
+		return nil
+	}
+
+	db, err := loadDatabase()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+	tx := db.MustBegin()
+	po.mappedPendingJobs.Range(func(key, value interface{}) bool {
+		pj := value.(*types.PendingJob)
+		timeDiff := time.Now().Sub(pj.UpdatedAt)
+		pj.Payload.RemainingSendAfter = pj.Payload.SendAfter - timeDiff
+		if _, err := tx.NamedExec("INSERT INTO pending_jobs (id, unique_id, payload, updated_at) VALUES (:id, :unique_id, :payload, :updated_at)", pj); err != nil {
+			log.Println(err)
+			return false
+		}
+		return true
+	})
+	return tx.Commit()
+}
+
+func gracefulShutdown(po *PostalOffice) {
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt)
+	signal.Notify(s, syscall.SIGTERM)
+	go func() {
+		<-s
+		// clean up here
+		if err := savePendingJobs(po); err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("Shutting down gracefully.")
+		os.Exit(0)
+	}()
 }
 
 func main() {
@@ -140,6 +302,10 @@ func main() {
 		mappedPendingJobs: &sync.Map{},
 	}
 
+	if err := loadPendingJobs(postalOffice); err != nil {
+		log.Println(err)
+	}
+
 	rpc.Register(postalOffice)
 	rpc.HandleHTTP()
 
@@ -148,5 +314,8 @@ func main() {
 		log.Fatal("listen error:", e)
 	}
 
-	http.Serve(l, nil)
+	go http.Serve(l, nil)
+	go gracefulShutdown(postalOffice)
+	forever := make(chan int)
+	<-forever
 }
--- postal_office/types/types.go
+++ postal_office/types/types.go
@@ -1,6 +1,11 @@
 package types
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"strings"
+	"time"
+)
 
 const DefaultEmailSendExp = 5 * time.Minute
 
@@ -11,6 +16,7 @@ const (
 )
 
 type NewJobArgs struct {
+	ID       string
 	Type     JobType
 	UniqueID string
 	After    time.Duration
@@ -18,10 +24,10 @@ type NewJobArgs struct {
 }
 
 type MailMessage struct {
-	Name    string
-	ToEmail string
-	Subject string
-	Content string
+	Name    string `json:"name"`
+	ToEmail string `json:"to_email"`
+	Subject string `json:"subject"`
+	Content string `json:"content"`
 }
 
 type CancelJobArgs struct {
@@ -30,4 +36,43 @@ type CancelJobArgs struct {
 
 type GetJobIDArgs struct {
 	UniqueID string
+}
+
+type JobPayload struct {
+	ParentJob          *PendingJob   `json:"-"`
+	Type               JobType       `json:"type"`
+	SendAfter          time.Duration `json:"send_after"`
+	RemainingSendAfter time.Duration `json:"remaining_send_after"`
+	Message            MailMessage   `json:"message"`
+}
+
+func (jp *JobPayload) Scan(value interface{}) error {
+	strJob, isString := value.(string)
+	if value == nil || !isString {
+		return nil
+	}
+
+	sr := strings.NewReader(strJob)
+	decodedJp := JobPayload{}
+	if err := json.NewDecoder(sr).Decode(&decodedJp); err != nil {
+		return err
+	}
+
+	*jp = decodedJp
+	return nil
+}
+
+func (jp *JobPayload) Value() (driver.Value, error) {
+	encodedJp, err := json.Marshal(jp)
+	if err != nil {
+		return nil, err
+	}
+	return string(encodedJp), nil
+}
+
+type PendingJob struct {
+	ID        string      `db:"id"`
+	UniqueID  string      `db:"unique_id"`
+	Payload   *JobPayload `db:"payload"`
+	UpdatedAt time.Time   `db:"updated_at"`
 }
