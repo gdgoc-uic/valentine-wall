268484b2a6fa17e7c90268fc8bee9b4d80dc9827
Ned Palacios <npdoesmc@gmail.com>
2022-01-30 14:03:32 +0800

    custom content for basic alert component

--- frontend/src/components/BasicAlert.vue
+++ frontend/src/components/BasicAlert.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="alert rounded-md justify-center" :class="{ 
+  <div class="alert rounded-md" :class="{ 
     'bg-orange-50 border-red-500 text-red-600': type == 'error', 
     'bg-blue-50 border-blue-500 text-blue-600': type == 'info', 
     'bg-green-50 border-teal-500 text-teal-600': type == 'success', 
     'bg-amber-50 border-orange-500 text-orange-600': type == 'warning' 
   }">
-    <p class="text-center">{{ message }}</p>
+    <slot>
+      <p class="mx-auto text-center">{{ message }}</p>
+    </slot>
   </div>
 </template>
 
