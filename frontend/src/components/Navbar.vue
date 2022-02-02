ea00a29e0e76f2da6fb3d516057b1dfe18e654b8
Ned Palacios <npdoesmc@gmail.com>
2022-02-02 12:11:03 +0800

    fix navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="[isHome ? 'pt-8' : 'py-8']" class="navbar mb-2 px-10 items-center bg-gradient-to-b from-[#FFEFEF] via-[#ffefef7e] to-transparent']">
+  <header :class="[isHome ? 'pt-8' : 'py-8']" class="navbar mb-2 px-10 items-center bg-gradient-to-b from-[#FFEFEF] via-[#ffefef7e] to-transparent']">
     <template v-if="!isHome">
       <!-- TODO: mobile view -->
       <!-- <div class="flex-none flex lg:hidden">
@@ -9,7 +9,7 @@
       </div> -->
       <router-link :to="{ name: 'home-page' }" class="flex-none flex-nowrap mr-8 lg:flex space-x-2">
         <img src="../assets/images/icon.png" class="h-full w-20" alt="Icon" />
-        <span class="flex-1 text-lg hidden lg:block"> Valentine<span class="font-bold">Wall</span> </span>
+        <span class="flex-1 text-lg hidden md:hidden lg:block"> Valentine<span class="font-bold">Wall</span> </span>
       </router-link>
       <search-form class="flex-none w-1/3">
         <div class="form-control p-2 bg-white shadow-md rounded-xl w-full">
@@ -39,8 +39,12 @@
         </div>
       </search-form>
     </template>
-    <div class="flex-none ml-auto">
-      <client-only>
+    <div :class="[isHome ? 'mx-auto' : 'ml-auto mr-8']" class="flex-none flex space-x-8">
+      <router-link :to="{ name: 'rankings-page' }" class="text-gray-600 hover:text-gray-800">Rankings</router-link>
+      <router-link :to="{ name: 'about-page' }" class="text-gray-600 hover:text-gray-800">About</router-link>
+    </div>
+    <client-only>
+      <div class="flex-none">
         <div v-if="$store.getters.isLoggedIn" class="dropdown dropdown-end dropdown-hover -mb-2">
           <div :class="{'rounded-r-none': !shouldSendButtonHide}" tabindex="0" class="mb-2 shadow-md btn px-8 normal-case text-black bg-white border-0 hover:bg-gray-100">
             <span>{{ $store.state.user.email }}</span>
@@ -48,8 +52,9 @@
           </div>
           <ul tabindex="0" class="p-2 shadow-md menu dropdown-content bg-base-100 rounded-box w-52">
             <li class="text-black"><router-link :to="{ name: 'settings-page' }">Settings</router-link></li>
-            <li class="text-black"><router-link :to="{ name: 'about-page' }">About</router-link></li>
-            <li class="text-red-500"><a @click.prevent="" class="cursor-pointer" @click="$store.dispatch('logout')">Logout</a></li>
+            <li class="text-red-500">
+              <a class="cursor-pointer" @click="logout">Logout</a>
+            </li>
           </ul>
         </div>
 
@@ -62,9 +67,9 @@
         </button>
 
         <login-button v-if="!isHome && !$store.getters.isLoggedIn" />
-      </client-only>
-    </div>
-  </div>
+      </div>
+    </client-only>
+  </header>
 </template>
 
 <script lang="ts">
@@ -72,7 +77,9 @@ import IconMenu from '~icons/uil/align-center-alt';
 import IconDropdown from '~icons/uil/angle-down';
 import IconSend from '~icons/uil/message';
 import IconSearch from '~icons/uil/search';
+import { catchAndNotifyError } from '../notify';
 import ClientOnly from './ClientOnly.vue';
+import LoginButton from './LoginButton.vue';
 import SearchForm from './SearchForm.vue';
 
 export default {
@@ -88,13 +95,24 @@ export default {
     IconSend,
     IconSearch,
     SearchForm,
-    ClientOnly
+    ClientOnly,
+    LoginButton
   },
   computed: {
     shouldSendButtonHide(): boolean {
       return this.isHome || this.$route.path.startsWith('/settings') || this.$route.path.startsWith('/about');
     }
   },
+  methods: {
+    async logout() {
+      try {
+        await this.$store.dispatch('logout');
+        this.$router.replace('/');
+      } catch(e) {
+        catchAndNotifyError(this, e);
+      }
+    }
+  }
 };
 </script>
 
