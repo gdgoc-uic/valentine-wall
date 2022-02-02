1058e9379054e9b0dd6587ab107e0ab4d0122a3d
Ned Palacios <npdoesmc@gmail.com>
2022-02-02 12:10:32 +0800

    remove vite-plugin-compress

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import Icons from 'unplugin-icons/vite'
 import checker from 'vite-plugin-checker'
 import { isoImport } from 'vite-plugin-iso-import'
 import markdown, { Mode } from 'vite-plugin-markdown'
-import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +16,5 @@ export default defineConfig({
     markdown({
       mode: [Mode.HTML, Mode.VUE]
     }),
-    // compress({
-    //   exclude: [
-    //     'ssr-manifest.json',
-    //     'index.html'
-    //   ]
-    // })
   ]
 })
