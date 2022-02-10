dfcb2ce41d6a9044783165a09bafe1d4a80938c8
Ned Palacios <npdoesmc@gmail.com>
2022-02-10 11:06:01 +0800

    use menu component to make settings list responsive

--- frontend/src/pages/Settings.vue
+++ frontend/src/pages/Settings.vue
@@ -3,16 +3,19 @@
     <h1 class="text-7xl font-bold text-rose-600 text-center self-center mb-12">Settings</h1>
 
     <article class="bg-white rounded-xl w-full shadow-lg">
-        <section class="rounded-t-xl bg-rose-50 flex pt-2">
-            <div class="tabs self-center mx-auto">
+        <section class="rounded-t-xl bg-rose-50 flex pt-2 md:pt-0">
+            <ul class="menu menu-vertical w-full md:w-auto md:menu-horizontal md:self-center md:mx-auto">
                 <router-link 
                     v-for="sectionRoute in settingSectionRoutes"
                     :key="sectionRoute.name"
                     :to="{ name: sectionRoute.name }" 
-                    exact-active-class="tab-active" 
-                    class="tab tab-lg tab-bordered"
-                    exact>{{ sectionRoute.meta.label }}</router-link>
-            </div>
+                    v-slot="{ href, route, navigate, isExactActive }"
+                    custom>
+                    <li :class="{ 'bordered': isExactActive }">
+                        <a @click="navigate" :href="href">{{ sectionRoute.meta.label }}</a>
+                    </li>
+                </router-link>
+            </ul>
         </section>
 
         <section class="px-8 lg:px-24 py-8">
