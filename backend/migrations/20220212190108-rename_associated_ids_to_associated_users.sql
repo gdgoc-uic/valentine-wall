44ef451f3ede3cc91401d4614b89742a6861ea92
Ned Palacios <npdoesmc@gmail.com>
2022-02-12 23:03:44 +0800

    fix sql

--- backend/migrations/20220212190108-rename_associated_ids_to_associated_users.sql
+++ backend/migrations/20220212190108-rename_associated_ids_to_associated_users.sql
@@ -1,6 +1,6 @@
 
 -- +migrate Up
-ALTER TABLE associated_users RENAME TO associated_users;
+ALTER TABLE associated_ids RENAME TO associated_users;
 
 -- +migrate Down
 ALTER TABLE associated_users RENAME TO associated_users;
