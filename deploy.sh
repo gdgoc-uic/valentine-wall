ff886a53cefec10988188fb78bfe59241dddf658
Ned Palacios <npdoesmc@gmail.com>
2023-02-11 23:40:36 +0800

    fix: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --no-deps --build -d $@
+if [ -x "$(command -v docker-compose)" ]; then
+  docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --no-deps --build -d $@
+else
+  docker compose -f docker-compose.yml -f docker-compose.prod.yml up --no-deps --build -d $@
+fi
