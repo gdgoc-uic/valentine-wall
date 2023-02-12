479ab9731a447b26618fc35f4bebb585b7fcc051
Ned Palacios <npdoesmc@gmail.com>
2023-02-12 14:00:03 +0800

    fix: loading messages

---
+++ backend/migrations/1676181578_updated_messages.go
@@ -0,0 +1,38 @@
+package migrations
+
+import (
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/tools/types"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection, err := dao.FindCollectionByNameOrId("caqiysan7yf0wve")
+		if err != nil {
+			return err
+		}
+
+		collection.ListRule = types.Pointer("gifts:length = 0 || (@request.auth.id = user.user.id || @request.auth.details.student_id = recipient)")
+
+		collection.ViewRule = types.Pointer("gifts:length = 0 || (@request.auth.id = user.user.id || @request.auth.details.student_id = recipient)")
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection, err := dao.FindCollectionByNameOrId("caqiysan7yf0wve")
+		if err != nil {
+			return err
+		}
+
+		collection.ListRule = nil
+
+		collection.ViewRule = nil
+
+		return dao.SaveCollection(collection)
+	})
+}
