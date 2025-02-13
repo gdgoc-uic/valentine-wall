585b4e0a0b4b3d35cdb4b8c7e7be3b66d455a7b0
Ralph Enrique <renrique_220000001478@uic.edu.ph>
2025-02-13 21:40:27 +0800

    valentine-wall-2025

--- frontend/src/components/GiftIcon.vue
+++ frontend/src/components/GiftIcon.vue
@@ -1,40 +1,52 @@
 <script>
 import { h } from 'vue';
 
-// Emojis
-import IconSunflower from '~icons/twemoji/sunflower';
-import IconRose from '~icons/twemoji/rose';
-import IconBalloon from '~icons/twemoji/balloon';
-import IconTeddyBear from '~icons/twemoji/teddy-bear';
-import IconRing from '~icons/twemoji/ring';
-import IconMoney from '~icons/twemoji/money-with-wings';
-import IconHeart from '~icons/twemoji/heart-with-ribbon';
-import IconChocolate from '~icons/twemoji/chocolate-bar';
-import IconPizza from '~icons/twemoji/pizza';
-import IconBubbleTea from '~icons/twemoji/bubble-tea';
+// Emojis (Icons as Vue components)
+
+// Import static image
+import sigenapls from '@/assets/images/gifts/sigenapls.png';
+import isforu from '@/assets/images/gifts/isforu.png';
+import isforyou from '@/assets/images/gifts/isforyou.png';
+import justin from '@/assets/images/gifts/justin_timberlake_throwing_a_rose.jpg.png';
+import meow from '@/assets/images/gifts/meow.png';
+import me_literally from '@/assets/images/gifts/me_literally.png';
+import rizzler from '@/assets/images/gifts/rizzler.png';
+import sili from '@/assets/images/gifts/sili.png';
+import batman from '@/assets/images/gifts/murag_batman.png';
 
 const emojis = {
-  'sunflower': IconSunflower,
-  'rose': IconRose,
-  'balloons': IconBalloon,
-  'teddy-bear': IconTeddyBear,
-  'ring': IconRing,
-  'money': IconMoney,
-  'heart': IconHeart,
-  'chocolate': IconChocolate,
-  'pizza': IconPizza,
-  'milk-tea': IconBubbleTea
+  sigenapls:  sigenapls,
+  isforu: isforu,
+  isforyou: isforyou,
+  timberlake: justin,
+  mukuha: meow,
+  me_literally: me_literally,
+  rizzler: rizzler,
+  sili: sili,
+  muragbatman : batman
 };
 
 export default {
-  render() {
-    return h(emojis[this.uid], this.$props);
-  },
   props: {
     uid: {
       type: String,
       required: true
     }
+  },
+  render() {
+    const emoji = emojis[this.uid];
+
+    // If it's a Vue component, render it
+    if (typeof emoji === 'object') {
+      return h(emoji, this.$props);
+    }
+
+    // If it's an image, render an <img> element
+    return h('img', {
+      src: emoji,
+      alt: this.uid,
+      ...this.$props
+    });
   }
-}
+};
 </script>
